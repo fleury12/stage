b78e07618128a31dcdfd68bb1609d16ec079e544
fleury12 <98595485+fleury12@users.noreply.github.com>
2024-04-06 02:48:29 +0200

    moi

---
+++ components/profil.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { IoCalendarNumber } from "react-icons/io5";
+import { Badge } from "@/components/ui/badge";
+import UserData from "@/lib/type";
+
+export const Profil = ({
+  firstName,
+  lastName,
+  description,
+  arrivalDate,
+  house,
+  assignment,
+  isTeacher,
+}: UserData) => {
+  let badgeColorClass = "";
+  let badgeText = house;
+  if (isTeacher && assignment) {
+    badgeText = assignment;
+  }
+  switch (house) {
+    case "Gryffindor":
+      badgeColorClass = "bg-red-500";
+      break;
+    case "Hufflepuff":
+      badgeColorClass = "bg-yellow-500";
+      break;
+    case "Ravenclaw":
+      badgeColorClass = "bg-blue-500";
+      break;
+    case "Slytherin":
+      badgeColorClass = "bg-green-500";
+      break;
+    default:
+      badgeColorClass = "bg-gray-500";
+      break;
+  }
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex flex-col sm:flex-row justify-between items-center w-full">
+          <div className="flex flex-col sm:flex-row items-center w-full">
+            <div>
+              <CardTitle className="text-3xs font-bold mb-2 sm:mb-0">
+                {firstName} {lastName}
+              </CardTitle>
+            </div>
+            <div className="sm:ml-8 flex-grow">
+              {" "}
+              <div className="flex justify-end w-full">
+                <Badge
+                  className={`text-white text-xs mr ${badgeColorClass}`}
+                  variant="outline"
+                >
+                  {badgeText}
+                </Badge>
+              </div>
+            </div>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="mr-8 text-2xs">
+        <p>{description}</p>
+      </CardContent>
+      <CardFooter className="text-xs">
+        <IoCalendarNumber />
+        <p>{arrivalDate}</p>
+      </CardFooter>
+    </Card>
+  );
+};
---
+++ lib/type.ts
@@ -0,0 +1,12 @@
+interface UserData {
+  id: number;
+  firstName: string;
+  lastName: string;
+  description: string;
+  arrivalDate: string;
+  house: string;
+  assignment?: string;
+  isTeacher: boolean;
+}
+
+export default UserData;
