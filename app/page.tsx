6f822d924a3d47c135a2e26e021ef16f76d75b10
fleury12 <98595485+fleury12@users.noreply.github.com>
2024-04-06 03:08:46 +0200

    moi

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,92 @@
+"use client";
+import React from "react";
 import Image from "next/image";
+import logo from "@/public/images/logo.png";
+import { Profil } from "@/components/profil";
+import UserData from "@/lib/type";
+import { useState, useEffect } from "react";
+import fichier_json from "@/public/files/profils.json";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const users: UserData[] = fichier_json;
+  console.log(users);
+  const compareArrivalDate = (a: UserData, b: UserData) => {
+    const dateA = new Date(a.arrivalDate);
+    const dateB = new Date(b.arrivalDate);
+    return dateA.getTime() - dateB.getTime();
+  };
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+  const teachers = users.filter((user) => user.isTeacher);
+  teachers.sort(compareArrivalDate);
+
+  const studentsByHouse: { [key: string]: UserData[] } = {};
+  users
+    .filter((user) => !user.isTeacher)
+    .sort((a, b) => a.firstName.localeCompare(b.firstName))
+    .forEach((student) => {
+      if (!studentsByHouse[student.house]) {
+        studentsByHouse[student.house] = [];
+      }
+      studentsByHouse[student.house].push(student);
+    });
+  const houses = Object.keys(studentsByHouse).reverse();
+
+  return (
+    <div className="m-10 ">
+      <div>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src={logo}
+          alt="logo de l'entreprise"
+          width={130}
+          height={130}
           priority
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col md:flex-row mt-8">
+        <div className="md:w-1/3 px-4 ">
+          <div className="font-bold text-xl"> Teachers </div>
+          <div className="mt-8">
+            {teachers.map((teacher) => (
+              <div key={teacher.id} className="mb-4">
+                <Profil
+                  firstName={teacher.firstName}
+                  lastName={teacher.lastName}
+                  description={teacher.description}
+                  arrivalDate={teacher.arrivalDate}
+                  house={teacher.house}
+                  assignment={teacher.assignment}
+                  isTeacher={teacher.isTeacher}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="md:w-2/3 px-4 ">
+          <div className="font-bold text-xl"> Students </div>
+          <div className="mt-8">
+            {houses.map((house) => (
+              <div key={house}>
+                <div className="flex flex-wrap">
+                  {studentsByHouse[house].map((student) => (
+                    <div key={student.id} className="sm:w-1/2 pr-4 mb-4">
+                      <Profil
+                        key={student.id}
+                        firstName={student.firstName}
+                        lastName={student.lastName}
+                        description={student.description}
+                        arrivalDate={student.arrivalDate}
+                        house={student.house}
+                        assignment={student.assignment}
+                        isTeacher={student.isTeacher}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
